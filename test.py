7e90217846b9ad1d0c512d9a5c7c2624caddb8b1
tejasramramesh <45738176+tejasramramesh@users.noreply.github.com>
2023-02-27 18:40:22 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+from linkml_runtime.dumpers import json_dumper
+from personinfo import Person
+
+p1 = Person(id='ORCID:9876', full_name='Lex Luthor', aliases=["Bad Guy"])
+print(json_dumper.dumps(p1))
